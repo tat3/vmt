f05ff089594017b0bbd6ba71128ff5d16b0135ac
tat3 <tat.github@gmail.com>
2020-04-15 13:30:33 +0000

    impl arithmetic

--- src/commandCompiler.ts
+++ src/commandCompiler.ts
@@ -1,4 +1,5 @@
 export class CommandCompiler {
+  jumpId = 0
 
   compilePush = (segment: string, index: number) => {
     return `    @${index}
@@ -7,8 +8,75 @@ export class CommandCompiler {
   }
 
   compileArithmetic = (operator: string) => {
-    return `
-    `
+    const popToD = this.compilePopToDRegister()
+    switch (operator) {
+      case 'add':
+        return `${popToD}
+    A=A-1
+    M=M+D`
+
+      case 'sub':
+        return `${popToD}
+    A=A-1
+    M=M-D`
+
+      case 'and':
+        return `${popToD}
+    A=A-1
+    M=M&D`
+
+      case 'or':
+        return `${popToD}
+    A=A-1
+    M=M|D`
+
+      case 'neg':
+        return `    @SP
+    A=M-1
+    M=-M`
+
+      case 'not':
+        return `    @SP
+    A=M-1
+    M=!M`
+
+      case 'eq':
+        return this.compileCompareArithmetic(operator)
+
+      case 'gt':
+        return this.compileCompareArithmetic(operator)
+
+      case 'lt':
+        return this.compileCompareArithmetic(operator)
+
+      default:
+        throw new Error(`invalid operator '${operator}'`)
+    }
+  }
+
+  compileCompareArithmetic = (operator: string) => {
+    if (operator !== 'eq' && operator !== 'gt' && operator !== 'lt') {
+      throw new Error(`argument of 'compileCompareArithmetic' must be eq, gt or lt`)
+    }
+    const popToD = this.compilePopToDRegister()
+    const jumpCmd = { 'eq': 'JEQ', 'gt': 'JGT', 'lt': 'JLT' }[operator]
+    const op = operator.toUpperCase()
+    const asm = `${popToD}
+    A=A-1
+    D=M-D
+    @${op}_${this.jumpId}
+    D;${jumpCmd}
+    D=0
+    @${op}_END_${this.jumpId}
+    0;JMP
+(${op}_${this.jumpId})
+    D=-1
+(${op}_END_${this.jumpId})
+    @SP
+    A=M
+    M=D`
+    this.jumpId++
+    return asm
   }
 
   compilePushFromDRegister = () => {
--- test/commandCompiler.test.ts
+++ test/commandCompiler.test.ts
@@ -48,10 +48,96 @@ describe('tokenizer test', () => {
   })
 
   it('generate arithmetic assembler code', () => {
-    expect(cc.compileArithmetic('add')).to.equal(`
-    `)
+    expect(cc.compileArithmetic('add')).to.equal(`    @SP
+    M=M-1
+    A=M
+    D=M
+    A=A-1
+    M=M+D`)
 
-    expect(cc.compileArithmetic('sub')).to.equal(`
-    `)
+    expect(cc.compileArithmetic('sub')).to.equal(`    @SP
+    M=M-1
+    A=M
+    D=M
+    A=A-1
+    M=M-D`)
+
+    expect(cc.compileArithmetic('and')).to.equal(`    @SP
+    M=M-1
+    A=M
+    D=M
+    A=A-1
+    M=M&D`)
+
+    expect(cc.compileArithmetic('or')).to.equal(`    @SP
+    M=M-1
+    A=M
+    D=M
+    A=A-1
+    M=M|D`)
+
+    expect(cc.compileArithmetic('neg')).to.equal(`    @SP
+    A=M-1
+    M=-M`)
+
+    expect(cc.compileArithmetic('not')).to.equal(`    @SP
+    A=M-1
+    M=!M`)
+
+    expect(cc.compileArithmetic('eq')).to.equal(`    @SP
+    M=M-1
+    A=M
+    D=M
+    A=A-1
+    D=M-D
+    @EQ_0
+    D;JEQ
+    D=0
+    @EQ_END_0
+    0;JMP
+(EQ_0)
+    D=-1
+(EQ_END_0)
+    @SP
+    A=M
+    M=D`)
+
+    expect(cc.compileArithmetic('gt')).to.equal(`    @SP
+    M=M-1
+    A=M
+    D=M
+    A=A-1
+    D=M-D
+    @GT_1
+    D;JGT
+    D=0
+    @GT_END_1
+    0;JMP
+(GT_1)
+    D=-1
+(GT_END_1)
+    @SP
+    A=M
+    M=D`)
+
+    expect(cc.compileArithmetic('lt')).to.equal(`    @SP
+    M=M-1
+    A=M
+    D=M
+    A=A-1
+    D=M-D
+    @LT_2
+    D;JLT
+    D=0
+    @LT_END_2
+    0;JMP
+(LT_2)
+    D=-1
+(LT_END_2)
+    @SP
+    A=M
+    M=D`)
+
+    expect(() => cc.compileArithmetic('aad')).to.throw('invalid operator \'aad\'')
   })
 })
